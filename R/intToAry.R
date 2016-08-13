15d7d9200ee053e66dd4a1db84e2ae39689f1be9
stla <laurent_step@yahoo.fr>
2016-08-13 12:40:47 +0200

    move ExpansionsR.so

--- R/intToAry.R
+++ R/intToAry.R
@@ -7,7 +7,12 @@
 #' @param n integer to be expanded
 #' @param sizes the base of the expansion
 #' @return A vector of length \code{prod(sizes)}, the expansion of \code{n}.
+#' @examples
+#' intToAry(77, c(3,4,7))
+#' 2*1 + 1*3 + 6*(3*4)
+#' # Cartesian product {0,1}x{0,1}:
+#' sapply(0:3, function(x) intToAry(x, sizes=c(2,2)))
 intToAry <- function(n, sizes){
-  if(n < 0 || n > prod(sizes)-1L) stop(sprintf("n must be nonnegative and strictly lower than %s (product of sizes)", prod(s)))
+  if(n < 0 || n > prod(sizes)-1L) stop(sprintf("n must be nonnegative and strictly lower than %s (product of sizes)", prod(sizes)))
   return(rcpp_intToAry(n, sizes))
 }
